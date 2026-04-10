7a9a0003029fe61be75f5e1bf2d933c2a16d10a4
Oliv-dev2908 <oliverradictos@gmail.com>
2026-04-10 15:23:35 -0400

    Update database.py

--- config/database.py
+++ config/database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
-import json
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("❌ DATABASE_URL no está definida en el .env")
 
 engine = create_engine(DATABASE_URL)
 
@@ -15,7 +19,6 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
