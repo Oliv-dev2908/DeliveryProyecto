90ee53fee7d9db60891c63d417153da7e99ce437
Oliv-dev2908 <oliverradictos@gmail.com>
2026-04-10 15:23:47 -0400

    Update firebase_config.py

--- config/firebase_config.py
+++ config/firebase_config.py
@@ -1,10 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials
+from dotenv import load_dotenv
 import os
 import json
 
+load_dotenv() 
+
 def init_firebase():
-    firebase_creds_json = json.loads(os.getenv("FIREBASE_CREDENTIALS"))
+    firebase_creds = os.getenv("FIREBASE_CREDENTIALS")
+
+    if not firebase_creds:
+        raise ValueError("❌ FIREBASE_CREDENTIALS no está en el .env")
+
+    firebase_creds_json = json.loads(firebase_creds)
 
     if not firebase_admin._apps:
         try:
